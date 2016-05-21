c399728128e0aa0746f5b3746aca0272f164a861
Željko Filipin <zeljko.filipin@gmail.com>
2016-05-21 11:23:09 +0200

    use "function" instead of hashrocket ("=>") in filtered ls

--- program.js
+++ program.js
@@ -4,7 +4,7 @@ var path = require('path');
 var directory = process.argv[2];
 var extension = '.' + process.argv[3];
 
-fs.readdir(directory, (err, list) => {
+fs.readdir(directory, function (err, list) {
   if (err) throw err;
   for (var i = 0; i < list.length; i++) {
     var filename = list[i];
