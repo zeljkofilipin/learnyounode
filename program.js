e7184a9bf6fb0dff32ff4158c38fc5132f7e7dba
Željko Filipin <zeljko.filipin@gmail.com>
2016-05-24 13:39:21 +0200

    http client

--- program.js
+++ program.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 var url = process.argv[2];
+var response = '';
 
 http.get(url, (res) => {
-  res.setEncoding('utf8')
-  res.on('data', console.log)
-}).on('error', console.error)
+  res.setEncoding('utf8');
+  res.on('data', (data) => {
+    response += data;
+  });
+  res.on('end', (data) => {
+    console.log(response.length);
+    console.log(response);
+  });
+}).on('error', console.error);
