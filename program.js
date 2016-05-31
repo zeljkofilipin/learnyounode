e9bf6b78b85a77636b9c4816d81e18a84e098054
Željko Filipin <zeljko.filipin@gmail.com>
2016-05-31 19:58:36 +0200

    Half done: HTTP JSON API server

--- program.js
+++ program.js
@@ -1,11 +1,30 @@
 const http = require('http');
-const map  = require('through2-map');
 const port = process.argv[2];
+const url  = require('url');
 
 const server = http.createServer((request, response) => {
-  request.pipe(map( (chunk) => {
-    return chunk.toString().toUpperCase();
-  })).pipe(response);
+  var pathname = url.parse(request.url, true).pathname;
+  var iso      = url.parse(request.url, true).query.iso;
+  console.log(new Date(iso));
+
+  parsetime = {  
+    "hour": 14,
+    "minute": 23,
+    "second": 15
+  } 
+
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+
+  if (pathname === '/api/parsetime') {
+    result = parsetime;
+  } else if (pathname === '/api/unixtime') {
+    var unixtime = new Date(iso).toISOString;
+    console.log(unixtime);
+    result = { "unixtime": unixtime }
+    console.log(result);
+  }
+
+  response.end(JSON.stringify(result));
 });
 
 server.listen(Number(port));
