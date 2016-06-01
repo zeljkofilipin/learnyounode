9a383483a3bbc74cab8eeae5769022d74ee7a4fa
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-01 14:20:10 +0200

    Finished HTTP JSON API server

--- program.js
+++ program.js
@@ -5,14 +5,21 @@ const url  = require('url');
 const server = http.createServer((request, response) => {
   var pathname = url.parse(request.url, true).pathname;
   var iso      = url.parse(request.url, true).query.iso;
-
-  parsetime = {  
-    "hour": 14,
-    "minute": 23,
-    "second": 15
-  } 
+  var result;
 
   if (pathname === '/api/parsetime') {
+    var hour   = new Date(iso).getHours();
+    var minute = new Date(iso).getMinutes();
+    var second = new Date(iso).getSeconds();
+
+    var parsetime = {  
+      "hour": hour,
+      "minute": minute,
+      "second": second 
+    } 
+
+    console.log(hour);
+
     result = parsetime;
   } else if (pathname === '/api/unixtime') {
     var unixtime = new Date(iso).getTime();
