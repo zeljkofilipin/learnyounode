aa15f482a4d8a1eb0695b6cbb98d7096ecb2feec
Željko Filipin <zeljko.filipin@gmail.com>
2016-06-01 14:22:50 +0200

    Minor cleanup of HTTP JSON API server

--- program.js
+++ program.js
@@ -2,23 +2,23 @@ const http = require('http');
 const port = process.argv[2];
 const url  = require('url');
 
+var result;
+
 const server = http.createServer((request, response) => {
   var pathname = url.parse(request.url, true).pathname;
   var iso      = url.parse(request.url, true).query.iso;
-  var result;
+  var date     = new Date(iso);
 
   if (pathname === '/api/parsetime') {
-    var hour   = new Date(iso).getHours();
-    var minute = new Date(iso).getMinutes();
-    var second = new Date(iso).getSeconds();
+    var hour   = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
 
     var parsetime = {  
       "hour": hour,
       "minute": minute,
       "second": second 
     } 
-
-    console.log(hour);
 
     result = parsetime;
   } else if (pathname === '/api/unixtime') {
